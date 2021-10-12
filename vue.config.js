03feecb1533e14977faa6880437c17c13d1afaa7
6238729 <6238729@gmail.com>
2021-10-12 13:10:29 +0900

    4 test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { NODE_ENV, BASE_URL = '/blogtest' } = process.env
+const { NODE_ENV, BASE_URL = '/' } = process.env
 
 module.exports = {
-  publicPath: NODE_ENV === 'production' ? BASE_URL : '/blogtest'
+  publicPath: NODE_ENV === 'production' ? BASE_URL : '/'
 }
