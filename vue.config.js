ba610c56e2b364ca9e0796efd8063599d5baecf6
6238729 <6238729@gmail.com>
2021-10-12 14:33:37 +0900

    initial commit for dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { NODE_ENV, BASE_URL = '/blogtest/' } = process.env
+ const { NODE_ENV, BASE_URL = '/blogtest/' } = process.env
 
 module.exports = {
   publicPath: NODE_ENV === 'production' ? BASE_URL : '/blogtest/'
